7345ebf4c22b689c447364e5a4069b7677a39403
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-15 09:49:53 -0800

    set up react-router for checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM A CHECKOUT, YEA BOI</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
