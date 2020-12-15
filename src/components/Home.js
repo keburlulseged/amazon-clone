9192f0a58351f9fee6f21539addb4ca139de3efc
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-14 23:57:44 -0800

    set up Home component and add promo banner

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="homeContainer">
+        <img
+          className="homeImage"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ODc1YjFhOTgt/ODc1YjFhOTgt-OTg3MDgxZGMt-w1500._CB413240536_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
