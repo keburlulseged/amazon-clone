3a3ad2ecaf64f969a77e491ddc5e32b23a5f7120
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-17 11:15:31 -0800

    set up express app and firebase cloud function

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51HzHI5H9oQuu69qk0ErpJXgtHxrKyjwckkVu8IFpqpcnjdnzYzIssb4SRQ6D3Si5qFiMIDxDxuPLmYJs5ukUOlEn00MIPvCmh8');
+
+// API
+
+// - App config
+const app = express();
+
+// - Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get('/', (request, response) => response.status(200).send('Yea Boi'));
+
+app.post('/payments/create')
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
