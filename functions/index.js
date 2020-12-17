854a119fea2e04d0266e044e3b3c208fb2edabed
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-17 11:46:30 -0800

    get clientSecret from stripe at checkout

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")('sk_test_51HzHI5H9oQuu69qk0ErpJXgtHxrKyjwckkVu8IFpqpcnjdnzYzIssb4SRQ6D3Si5qFiMIDxDxuPLmYJs5ukUOlEn00MIPvCmh8');
+const stripe = require("stripe")(
+  "sk_test_51HzHI5H9oQuu69qk0ErpJXgtHxrKyjwckkVu8IFpqpcnjdnzYzIssb4SRQ6D3Si5qFiMIDxDxuPLmYJs5ukUOlEn00MIPvCmh8"
+);
 
 // API
 
@@ -13,9 +15,21 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 // - API routes
-app.get('/', (request, response) => response.status(200).send('Yea Boi'));
+app.get("/", (request, response) => response.status(200).send("Yea Boi"));
 
-app.post('/payments/create')
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment request recieved Boi!", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of currency
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 // - Listen command
 exports.api = functions.https.onRequest(app);
